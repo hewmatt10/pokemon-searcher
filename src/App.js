2efd581dc9db379c947edce2948cff55b6634f4b
hewmatt10 <56776292+hewmatt10@users.noreply.github.com>
2022-07-30 23:20:31 -0400

    updated search functionalities

--- src/App.js
+++ src/App.js
@@ -22,15 +22,50 @@ class App extends Component {
       fetchedPokemon[i].id = i+1;
       let name = fetchedPokemon[i].name
       name = name.charAt(0).toUpperCase() + name.slice(1);
-      if (name.includes('n-m')) {
+
+      if (name.includes('-')) console.log(name);
+
+      if (name.includes('Tapu-')) { // Tapu cases
+        name = name.split('-')
+        name = name.map((part) => {
+          return part.charAt(0).toUpperCase() + part.slice(1);
+        })
+        name = name.join(' ');
+      }
+      else if (name.includes('Mr-')) { // Mr. Mime and Mr. Rime Cases
+        name = name.split('-')
+        name = name.map((part) => {
+          return part.charAt(0).toUpperCase() + part.slice(1);
+        })
+        name[0] = name[0] + '.';
+        name = name.join(' ');
+      }
+      else if (name.includes('n-m')) {
         name = name.replace('-m', '♂'); // Nidoran♂ Case
       }
       else if(name.includes('n-f')) {
         name = name.replace('-f', '♀'); // Nidoran♀ Case
       }
-      else if(name.includes('-m')) {
-        name = name.replace('-m', '. M'); // Mr. Mime Case
+      else if(name.includes('-z')) {
+        name = name.replace('z', 'Z'); // Porygon-Z Case
       }
+      else if(name.includes('-jr')) {
+        name = name.replace('-jr', ' Jr.'); // Mime Jr. Case
+      }
+      else if(name.includes('Type-')) { // Type: Null Case
+        name = name.split('-')
+        name = name.map((part) => {
+          return part.charAt(0).toUpperCase() + part.slice(1);
+        })
+        name[0] = name[0] + ':';
+        name = name.join(' ');
+      }
+      else if(name === 'Ho-oh'){
+        name = name.split('')
+        name[3] = 'O';
+        name = name.join('');
+      }
+      
       fetchedPokemon[i].name = name;
     }
     console.log(fetchedPokemon)
@@ -39,7 +74,7 @@ class App extends Component {
     })
   }
   onSearch = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase();
+    const searchField = event.target.value.toLocaleLowerCase().replace(/[^a-z0-9]/gi, '');
     this.setState({
       searchField,
     })
@@ -49,7 +84,7 @@ class App extends Component {
     const {pokemon, searchField} = this.state;
     const {onSearch} = this;
     const filteredPokemon = pokemon.filter((pokemon) => {
-      return pokemon.name.toLocaleLowerCase().includes(searchField);
+      return pokemon.name.toLocaleLowerCase().replace(/[^a-z0-9]/gi, '').includes(searchField);
     })
     return (
       <div className="App">
